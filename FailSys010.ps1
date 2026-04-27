2e886d6be9a92402eedc14d235a7af282710114c
ぴょん吉 <pyontana@gmail.com>
2026-04-27 11:19:42 +0900

    Update

--- FailSys010.ps1
+++ FailSys010.ps1
@@ -1,3 +1,5 @@
 $pythonPath = "C:\Users\tanakak\AppData\Local\Programs\Python\Python313"    					# Pythonが入っているパス
-$scriptFile = "FailSys010.py"                                               					# スクリプトファイルの名前
+$scriptFile1 = "FailSys010.py"                                               					# スクリプト
+$scriptFile2 = "FailSys020.py"                                               					# スクリプトファイルの名前
 powershell.exe -NoProfile -ExecutionPolicy Bypass "& '$pythonPath\python.exe' '$PSScriptRoot\$scriptFile' up"
+powershell.exe -NoProfile -ExecutionPolicy Bypass "& '$pythonPath\python.exe' '$PSScriptRoot\$scriptFile' production"
